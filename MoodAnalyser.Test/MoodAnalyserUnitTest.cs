068223fbd68720981a389e34861a2038c54af207
Saksham27 <geekysaksham27@gmail.com>
2020-04-20 11:40:33 +0530

    Test Case 1.2 : Given any mood message should return happy

--- MoodAnalyser.Test/MoodAnalyserUnitTest.cs
+++ MoodAnalyser.Test/MoodAnalyserUnitTest.cs
@@ -20,5 +20,16 @@ namespace MoodAnalyser.Test
             string mood = moodAnalyser.AnalyseMood(message);
             Assert.AreEqual(expected, mood);
         }
+
+        // Test Case 1.2
+        [Test]
+        public void GivenAnyMood_ShouldReturnHappy()
+        {
+            string expected = "Happy";
+            string message = "I am in any mood";
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string mood = moodAnalyser.AnalyseMood(message);
+            Assert.AreEqual(expected, mood);
+        }
     }
 }
