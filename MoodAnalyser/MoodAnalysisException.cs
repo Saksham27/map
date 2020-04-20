8238ed01dfd7bf26ffdb60d6ce7122620d97a6a0
Saksham27 <geekysaksham27@gmail.com>
2020-04-20 14:27:25 +0530

    Added: created MoodAnalysisException to throw custom exception

---
+++ MoodAnalyser/MoodAnalysisException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    class MoodAnalysisException : Exception
+    {
+        private ExceptionType _ExceptionType;
+        public enum ExceptionType
+        {
+            EnteredNull
+        }
+        
+
+        public MoodAnalysisException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this._ExceptionType = 
+        }
+    }
+}
