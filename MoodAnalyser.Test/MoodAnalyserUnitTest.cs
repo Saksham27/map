cf9cd3a5f121ffe46aa49bc1c6bb30926cf8e104
Saksham27 <geekysaksham27@gmail.com>
2020-04-20 11:58:11 +0530

    Refactor: Refactor Test Case 1.1

--- MoodAnalyser.Test/MoodAnalyserUnitTest.cs
+++ MoodAnalyser.Test/MoodAnalyserUnitTest.cs
@@ -16,20 +16,11 @@ namespace MoodAnalyser.Test
         {
             string expected = "Sad";
             string message = "I am in Sad mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string mood = moodAnalyser.AnalyseMood(message);
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string mood = moodAnalyser.AnalyseMood();
             Assert.AreEqual(expected, mood);
         }
 
-        // Test Case 1.2
-        [Test]
-        public void GivenAnyMood_ShouldReturnHappy()
-        {
-            string expected = "Happy";
-            string message = "I am in any mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string mood = moodAnalyser.AnalyseMood(message);
-            Assert.AreEqual(expected, mood);
-        }
+       
     }
 }
