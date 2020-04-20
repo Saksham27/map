3a49ee410717fab58da8c7ee407bb29936617ec1
Saksham27 <geekysaksham27@gmail.com>
2020-04-20 11:23:45 +0530

    Added: mood analysis function

--- MoodAnalyser/MoodAnalyser.cs
+++ MoodAnalyser/MoodAnalyser.cs
@@ -4,9 +4,23 @@ namespace MoodAnalyser
 {
     class MoodAnalyser
     {
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                return "Sad Mood";
+            else 
+                return "Happy Mood";
+        }
+
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"> no parameters </param>
         static void Main(string[] args)
         {
-            
+            // creating moood analyser object
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string mood = moodAnalyser.AnalyseMood("I am in Happy mood");
         }
     }
 }
